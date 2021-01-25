423d9eeb0b5e2df08e4c82abe1ca4bd77d08e360
claireyywang <22240514+claireyywang@users.noreply.github.com>
2021-01-24 16:08:44 -0800

    get port
    Signed-off-by: claireyywang <22240514+claireyywang@users.noreply.github.com>

--- src/main.go
+++ src/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
+	"os"
 )
 
 // placeholder front page
@@ -12,6 +14,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT is not set.")
+	}
+	portStr := strings.Join([]string{":", port}, "")
 	http.HandleFunc("/", handler) // placeholder front page
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(portStr, nil))
 }
