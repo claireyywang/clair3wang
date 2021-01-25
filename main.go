3ce4b32a148195279e719b2f8b5d668d08260add
claireyywang <22240514+claireyywang@users.noreply.github.com>
2021-01-25 00:16:50 -0800

    add nested templates

--- main.go
+++ main.go
@@ -1,22 +1,49 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+
+	"github.com/russross/blackfriday/v2"
 )
 
 // placeholder front page
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, it's me.")
+	input, err := ioutil.ReadFile("markdown/index.md")
+	if err != nil {
+		log.Fatal(err)
+	}
+	md := blackfriday.Run(input)
+	w.Write(md)
+}
+
+// nest blog.html into layout.html
+func nestTemplate(w http.ResponseWriter, r *http.Request) {
+	lp := filepath.Join("templates", "layout.html")
+	// sanitize untrusted user request
+	// [TODO] further sanitise before joining untrusted input
+	// to prevent directory traversal attack
+	fp := filepath.Join("templates", filepath.Clean(r.URL.Path))
+
+	tmpl, _ := template.ParseFiles(lp, fp)
+	tmpl.ExecuteTemplate(w, "layout", nil)
 }
 
 func main() {
+	fs := http.FileServer(http.Dir("./blogs"))
+	http.Handle("/blogs/", http.StripPrefix("/blogs/", fs))
+
+	http.HandleFunc("/", nestTemplate)
+
+	// set dynamic port number for heroku deployment
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("$PORT is not set.")
 	}
-	http.HandleFunc("/", handler) // placeholder front page
+	// deploy to port
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
