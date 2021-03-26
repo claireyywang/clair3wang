d62404d2d13ced00277d829bad5a65f9bb85b815
claireyywang <22240514+claireyywang@users.noreply.github.com>
2021-03-26 00:04:50 -0700

    update main package
    Signed-off-by: claireyywang <22240514+claireyywang@users.noreply.github.com>

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	// "html/template"
+	"log"
+	"net/http"
+	"os"
+	// "path/filepath"
+)
+
+// home Home page handler
+// url "/"
+func home(w http.ResponseWriter, r *http.Request) {
+	// lp := filepath.Join("templates", "layout.html")
+	// fp := filepath.Join("templates", filepath.Clean(r.URL.Path))
+
+	// tmpl, _ := template.ParseFiles(lp, fp)
+	// tmpl.ExecuteTemplate(w, "layout", nil)
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Hello from Claire"))
+}
+
+// showCode Code page handler
+// url "/code"
+func showCode(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display Code section ..."))
+}
+
+// showEat Eat page handler
+// url "/eat"
+func showEat(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display Eat section ..."))
+}
+
+// showSleep Sleep page handler
+// url "/sleep"
+func showSleep(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display Sleep section ..."))
+}
+
+func main() {
+	// mux treats "/" like catch-all "/foo" also shows home()
+	// http.HandleFunc() uses DefaultServeMux, which is a global variable
+	// use locally scoped mux for security
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/code", showCode)
+	mux.HandleFunc("/eat", showEat)
+	mux.HandleFunc("/sleep", showSleep)
+
+	// set dynamic port number for heroku deployment
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT is not set.")
+	}
+
+	log.Println("Starting server on :"+port)
+	err := http.ListenAndServe(":"+port, mux)
+	log.Fatal(err)
+}
--- server.go
+++ server.go
@@ -1,33 +1,33 @@
-package main
+// package main
 
-import (
-	"html/template"
-	"log"
-	"net/http"
-	"os"
-	"path/filepath"
-)
+// import (
+// 	"html/template"
+// 	"log"
+// 	"net/http"
+// 	"os"
+// 	"path/filepath"
+// )
 
-/*
+// /*
 
-*/
-func serveTemplate(w http.ResponseWriter, r *http.Request) {
-	lp := filepath.Join("templates", "layout.html")
-	fp := filepath.Join("templates", filepath.Clean(r.URL.Path))
+// */
+// func serveTemplate(w http.ResponseWriter, r *http.Request) {
+// 	lp := filepath.Join("templates", "layout.html")
+// 	fp := filepath.Join("templates", filepath.Clean(r.URL.Path))
 
-	tmpl, _ := template.ParseFiles(lp, fp)
-	tmpl.ExecuteTemplate(w, "layout", nil)
-}
+// 	tmpl, _ := template.ParseFiles(lp, fp)
+// 	tmpl.ExecuteTemplate(w, "layout", nil)
+// }
 
-func main() {
-	// set up home page
-	http.HandleFunc("/", serveTemplate)
+// func main() {
+// 	// set up home page
+// 	http.HandleFunc("/", serveTemplate)
 
-	// set dynamic port number for heroku deployment
-	port := os.Getenv("PORT")
-	if port == "" {
-		log.Fatal("$PORT is not set.")
-	}
-	// deploy to port
-	log.Fatal(http.ListenAndServe(":"+port, nil))
-}
+// 	// set dynamic port number for heroku deployment
+// 	port := os.Getenv("PORT")
+// 	if port == "" {
+// 		log.Fatal("$PORT is not set.")
+// 	}
+// 	// deploy to port
+// 	log.Fatal(http.ListenAndServe(":"+port, nil))
+// }
