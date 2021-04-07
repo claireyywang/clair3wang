513517b5b6b9cf24daecb293121253599b217fc5
claireyywang <22240514+claireyywang@users.noreply.github.com>
2021-04-07 14:51:48 -0700

    isolate routes
    Signed-off-by: claireyywang <22240514+claireyywang@users.noreply.github.com>

--- app/web/main.go
+++ app/web/main.go
@@ -14,6 +14,7 @@ type application struct {
 	infoLog *log.Logger
 }
 
+// main parse runtin config, establish dependencies, run HTTP server
 func main() {
 	// leveled logging
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
@@ -24,19 +25,7 @@ func main() {
 		errorLog: errorLog,
 		infoLog: infoLog,
 	}
-	// mux treats "/" like catch-all "/foo" also shows home()
-	// http.HandleFunc() uses DefaultServeMux, which is a global variable
-	// use locally scoped mux for security
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/code", app.showCode)
-	mux.HandleFunc("/eat", showEat)
-	mux.HandleFunc("/sleep", showSleep)
-
-	// create a file serve which serves files out of ./ui/static dir
-	// path given to http.Dir is relative to project root
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	
 
 	// set dynamic port number for heroku deployment
 	port := ":"+os.Getenv("PORT")
@@ -46,13 +35,14 @@ func main() {
 
 	// initiate a new http.Server struct to use errorlog
 	addr := flag.String("addr", port, "HTTP network address")
+	flag.Parse()
 	srv := &http.Server{
 		Addr: *addr,
 		ErrorLog: errorLog, 
-		Handler: mux,
+		Handler: app.routes(),
 	}
 
-	infoLog.Printf("Starting server on :"+port)
+	infoLog.Printf("Starting server on %s", *addr)
 	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
---
+++ app/web/routes.go
@@ -0,0 +1,21 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+	// mux treats "/" like catch-all "/foo" also shows home()
+	// http.HandleFunc() uses DefaultServeMux, which is a global variable
+	// use locally scoped mux for security
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/code", app.showCode)
+	mux.HandleFunc("/eat", showEat)
+	mux.HandleFunc("/sleep", showSleep)
+
+	// create a file serve which serves files out of ./ui/static dir
+	// path given to http.Dir is relative to project root
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	return mux
+}
