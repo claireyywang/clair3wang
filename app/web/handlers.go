3bed9f6db6eb86cf13ab1263b98ef2cec3d203e0
claireyywang <22240514+claireyywang@users.noreply.github.com>
2021-04-17 13:42:55 -0700

    add html and blackfriday
    Signed-off-by: claireyywang <22240514+claireyywang@users.noreply.github.com>

--- app/web/handlers.go
+++ app/web/handlers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"html/template"
+	"github.com/russross/blackfriday"
 )
 
 // createPage create page with templates
@@ -12,8 +13,8 @@ func (app *application) createPage(w http.ResponseWriter, fileName string) {
 	// or an abosolute path 
 	files := []string{
 		fileName,
-		"./ui/html/base.layout.tmpl",
-		"./ui/html/footer.partial.tmpl",
+		"./ui/html/base.layout.tmpl.html",
+		"./ui/html/footer.partial.tmpl.html",
 	}
 	// use template.ParseFiles() to read tmpl files
 	// the slice of template files are passed as a variadic parameter
@@ -38,23 +39,25 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// create and execute the templated page
-	app.createPage(w, "./ui/html/home.page.tmpl")
+	app.createPage(w, "./ui/html/home.page.tmpl.html")
 }
 
 // showCode Code page handler
 // url "/code"
 func (app *application) showWork(w http.ResponseWriter, r *http.Request) {
-	app.createPage(w, "./ui/html/work.page.tmpl")
+	// app.createPage(w, "./ui/html/work.page.tmpl")
+
+
 }
 
 // showEat Eat page handler
 // url "/eat"
 func (app *application) showLife(w http.ResponseWriter, r *http.Request) {
-	app.createPage(w, "./ui/html/life.page.tmpl")
+	app.createPage(w, "./ui/html/life.page.tmpl.html")
 }
 
 // showSleep Sleep page handler
 // url "/sleep"
 func (app *application) showContact(w http.ResponseWriter, r *http.Request) {
-	app.createPage(w, "./ui/html/contact.page.tmpl")
+	app.createPage(w, "./ui/html/contact.page.tmpl.html")
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/claireyywang/my-hobby-site.git
 
 go 1.15
 
-require github.com/russross/blackfriday/v2 v2.1.0
+require (
+	github.com/russross/blackfriday v1.6.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0
+)
