bb1660a58de6d0a3a7ed4b4064c75ebbb34ebe05
claireyywang <22240514+claireyywang@users.noreply.github.com>
2021-05-23 13:57:16 -0700

    modify window size
    Signed-off-by: claireyywang <22240514+claireyywang@users.noreply.github.com>

--- ui/static/js/art.js
+++ ui/static/js/art.js
@@ -6,6 +6,7 @@ const height = width / 1.26;
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, width / height , 0.1, 1000 );
+camera.position.z = 5;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(width, height);
@@ -15,12 +16,11 @@ window.addEventListener("resize", function() {
 });
 container.appendChild( renderer.domElement );
 
+// create cube
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
-
-camera.position.z = 5;
 
 const animate = function () {
     requestAnimationFrame( animate );
