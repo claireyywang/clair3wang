5d73d336b9d6451c76cd9bedec019cf4e69da452
claireyywang <22240514+claireyywang@users.noreply.github.com>
2021-05-28 22:56:53 -0700

    allow cubes resize and improve res
    Signed-off-by: claireyywang <22240514+claireyywang@users.noreply.github.com>

--- ui/static/js/cubes.js
+++ ui/static/js/cubes.js
@@ -1,45 +1,39 @@
-import * as THREE from 'https://cdn.skypack.dev/three@0.128.0'
+import * as THREE from 'https://cdn.skypack.dev/three@0.128.0/build/three.module.js'
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/controls/OrbitControls.js'
 
 function main() {
     // canvas
     const canvas = document.querySelector("#art");
-    const width = window.innerWidth * 0.75;
-    const height = width / 1.26;
 
     // renderer
     const renderer = new THREE.WebGLRenderer({canvas});
-    renderer.setSize(width, height);
-    // resize when window size changes
-    window.addEventListener("resize", function() {
-        renderer.setSize(width, height);
-    });
 
     // camera
     const fov = 75;
-    // const aspect = 2;
+    const aspect = 2;
     const near = 0.1;
     const far = 5;
-    const camera = new THREE.PerspectiveCamera( fov, width/height, near, far );
+    const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
     camera.position.z = 2;
 
     // scene
     const scene = new THREE.Scene();
     scene.background = new THREE.Color('azure');
 
-    // box
-    const boxWidth = .5;
-    const boxHeight = .5;
-    const boxDepth = .5;
-    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-
-    // light
-    {
+     // light
+     {
         const color = 0xffffff;
         const intensity = 1;
         const light = new THREE.DirectionalLight(color, intensity);
         light.position.set(-1, 2, 4);
         scene.add(light);
     }
+
+    // box
+    const boxWidth = .5;
+    const boxHeight = .5;
+    const boxDepth = .5;
+    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
     function makeObject(geometry, color, x) {
         const material = new THREE.MeshPhongMaterial({color});
@@ -55,8 +49,26 @@ function main() {
         makeObject(geometry, 0x4169e1, 1),
     ]
 
+    function resizeRendererToDisplaySize(renderer) {
+        const canvas = renderer.domElement;
+        const pixelRatio = window.devicePixelRatio;
+        const width = canvas.clientWidth * pixelRatio | 0;
+        const height = canvas.clientHeight * pixelRatio | 0;
+        const needResize = canvas.width !== width || canvas.height !== height;
+        if (needResize) {
+            renderer.setSize(width, height, false);
+        }
+        return needResize;
+    }
+
     function render(time) {
         time *= 0.001;
+
+        if (resizeRendererToDisplaySize(renderer)) {
+            const canvas = renderer.domElement;
+            camera.aspect = canvas.clientWidth / canvas.clientHeight;
+            camera.updateProjectionMatrix();
+        }
 
         cubes.forEach((cube, ndx) => {
             const speed = 1 + ndx * .1;
